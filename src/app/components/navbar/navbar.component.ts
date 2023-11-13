0c916848852b932968ff3ee0b2130e2abff96dfb
Jose_Luis <joselu2004.jlrc@gmail.com>
2023-11-13 22:24:21 +0100

    ya te puedes registrar y logear segun base de datos

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,10 +10,26 @@ import { LoginService } from '../../services/login.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  darkMode: boolean = false;
+
   constructor(public loginS: LoginService) { }
   ngOnInit(): void {
   }
   public logout() {
     this.loginS.signOut();
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+
+    // Puedes cambiar la lógica según tus necesidades
+    if (this.darkMode) {
+      // Aplicar estilos o clases para el modo oscuro
+      document.body.classList.add('dark-mode');
+    } else {
+      // Eliminar estilos o clases para volver al modo claro
+      document.body.classList.remove('dark-mode');
+    }
   }
 }
